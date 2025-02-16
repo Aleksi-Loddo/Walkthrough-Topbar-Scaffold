3c0a8040b4b93a2b2ac2fb7ca51232b7c1c6fd8a
aleksi loddo <143383551+Aleksi-Loddo@users.noreply.github.com>
2025-02-16 15:27:01 +0200

    ScaffoldApp file commit

---
+++ app/src/main/java/com/example/walkthrough_topbar_ui/ScaffoldApp.kt
@@ -0,0 +1,25 @@
+package com.example.walkthrough_topbar_ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.walkthrough_topbar_ui.ui.InfoScreen
+import com.example.walkthrough_topbar_ui.ui.MainScreen
+import com.example.walkthrough_topbar_ui.ui.SettingsScreen
+
+
+@Composable
+fun ScaffoldApp() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable(route = "home") { MainScreen(navController) }
+        composable(route = "info") { InfoScreen(navController) }
+        composable(route = "settings") { SettingsScreen(navController) }
+    }
+}
+
+
